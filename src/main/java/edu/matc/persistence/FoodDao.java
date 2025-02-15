7e3a50f16a6ac51c83d2535af5dba802cf062351
aburgy-93 <aburgy@uwalumni.com>
2025-02-15 12:43:38 -0600

    Added the user table to the database connection. Added the User entity and the UserDao. Fixed some configuration issues when copying over set up files from hibernate demos

--- src/main/java/edu/matc/persistence/FoodDao.java
+++ src/main/java/edu/matc/persistence/FoodDao.java
@@ -1,4 +1,21 @@
 package edu.matc.persistence;
 
+import org.hibernate.SessionFactory;
+
+import edu.matc.entity.Food;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Root;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+
 public class FoodDao {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+
 }
