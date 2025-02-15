9a2b6df39c7e8a4bc88965ecf6389f2c71b3545b
aburgy-93 <aburgy@uwalumni.com>
2025-02-15 15:17:06 -0600

    Set up the UserDoa

--- src/main/java/edu/matc/persistence/UserDao.java
+++ src/main/java/edu/matc/persistence/UserDao.java
@@ -1,12 +1,88 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.User;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+
+import java.util.List;
 
 public class UserDao {
     private final Logger logger = LogManager.getLogger(this.getClass());
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
+    /**
+     * Get user by id
+     *
+     * @param id user's id
+     */
 
+    public User getUserById(int id) {
+        Session session = sessionFactory.openSession();
+        User user = session.get(User.class, id);
+        session.close();
+        return user;
+    }
+
+    /**
+     * update user
+     * @param user User to be updated
+     */
+    public void updateUser(User user) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.merge(user);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * @param user user to be added
+     * @return id
+     */
+    public int addUser(User user) {
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(user);
+        transaction.commit();
+        id = user.getId();
+        session.close();
+        return id;
+    }
+
+    /**
+     * Delete a user
+     * @param user user to be deleted
+     */
+    public void deleteUser(User user) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * Return a list of all users
+     * @return All users
+     */
+    public List<User> getAllUsers() {
+        Session session = sessionFactory.openSession();
+
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        List<User> users = session.createSelectionQuery( query ).getResultList();
+
+        logger.debug("The lis of users " + users);
+        session.close();
+
+        return users;
+    }
 }
