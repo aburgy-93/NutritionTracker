99b99db2df8966733ffa69340df25eefb45d3827
aburgy-93 <aburgy@uwalumni.com>
2025-02-16 11:39:49 -0600

    Set up getters and setters in the Food class

--- src/main/java/edu/matc/entity/Food.java
+++ src/main/java/edu/matc/entity/Food.java
@@ -56,4 +56,99 @@ public class Food {
         this.protein = protein;
     }
 
+    /**
+     * Gets name of food
+     * @return the food name
+     */
+    public String getFoodName() {return foodName;}
+
+    /**
+     * Sets the food name
+     * @param foodName the name of the food
+     */
+    public void setFoodName(String foodName) {this.foodName = foodName;};
+
+    /**
+     * Gets a food type
+     * @return food type
+     */
+    public String getFoodType() {return foodType;}
+
+    /**
+     * Sets the food type
+     * @param foodType the food type
+     */
+    public void setFoodType(String foodType) {this.foodType = foodType;};
+
+    /**
+     * Gets the serving size
+     * @return serving size
+     */
+    public int getServingSize() {return servingSize;}
+
+    /**
+     * Set the serving size
+     * @param servingSize the serving size
+     */
+    public void setServingSize(int servingSize) {this.servingSize = servingSize;};
+
+    /**
+     * Gets the serving unit
+     * @return serving unit
+     */
+    public String getServingUnit() {return servingUnit;}
+
+    /**
+     * Set serving unit
+     * @param servingUnit serving unit
+     */
+    public void setServingUnit(String servingUnit) {this.servingUnit = servingUnit;};
+
+    /**
+     * Gets calories of food
+     * @return calories of food
+     */
+    public int getCalories() {return calories;}
+
+    /**
+     * Sets the calories of the food
+     * @param calories calories in food
+     */
+    public void setCalories(int calories) {this.calories = calories;};
+
+    /**
+     * Gets fat in food
+     * @return fat in food
+     */
+    public int getFat(){return fat;};
+
+    /**
+     * Sets the fat amount in a food
+     * @param fat amount of fat in food
+     */
+    public void setFat(int fat) {this.fat = fat;}
+
+    /**
+     * Get the number of carbs in food
+     * @return number of carbs in food
+     */
+    public int getCarbs(){return carbs;}
+
+    /**
+     * Sets the number of carbs in food
+     * @param carbs the number of carbs in food
+     */
+    public void setCarbs(int carbs) {this.carbs = carbs;}
+
+    /**
+     * Get the amount of protein in food
+     * @return amount of protein in food
+     */
+    public int getProtein(){return protein;}
+
+    /**
+     * Set the protein amount in food
+     * @param protein protein amount in food
+     */
+    public void setProtein(int protein) {this.protein = protein;}
 }
