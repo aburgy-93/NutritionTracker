71d620b2e4a89a8f454c66a4e1889524f4eff2a9
aburgy-93 <aburgy@uwalumni.com>
2025-02-16 11:48:43 -0600

    Added a get and set id function to Food user and set up FoodDao

--- src/main/java/edu/matc/persistence/FoodDao.java
+++ src/main/java/edu/matc/persistence/FoodDao.java
@@ -1,4 +1,62 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.Food;
+import jakarta.persistence.criteria.CriteriaQuery;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+
+import java.util.List;
+
 public class FoodDao {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    public Food getFoodById(int id){
+        Session session = sessionFactory.openSession();
+        Food food = session.get(Food.class, id);
+        session.close();
+        return food;
+    }
+
+    public List<Food> getAllFood(){
+        Session session = sessionFactory.openSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Food> criteria = builder.createQuery(Food.class);
+        List<Food> foods = session.createSelectionQuery(criteria).getResultList();
+
+        logger.debug("The list of foods is " + foods);
+        session.close();
+        return foods;
+    }
+
+    public void updateFood(Food food){
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.merge(food);
+        transaction.commit();
+        session.close();
+    }
+
+    public int insertFood(Food food){
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(food);
+        transaction.commit();
+        id = food.getId();
+        session.close();
+        return id;
+    }
+
+    public void deleteFood(Food food){
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(food);
+        transaction.commit();
+        session.close();
+    }
 }
