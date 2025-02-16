a4cd9cc414114232939d97a1305aa0e20658504f
aburgy-93 <aburgy@uwalumni.com>
2025-02-16 12:57:12 -0600

    Set up UserFoodDao and created test class for it

--- src/main/java/edu/matc/persistence/UserFoodDao.java
+++ src/main/java/edu/matc/persistence/UserFoodDao.java
@@ -1,5 +1,8 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.User;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.SessionFactory;
 
 import edu.matc.entity.UserFood;
@@ -7,6 +10,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+
+import java.util.List;
 
 public class UserFoodDao {
     private final Logger logger = LogManager.getLogger(this.getClass());
@@ -22,8 +28,17 @@ public class UserFoodDao {
         return food;
     }
 
-    public int insertUserFood(UserFood food) {
+    public void updateUserFood(UserFood food) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.merge(food);
+        transaction.commit();
+        session.close();
+    }
+
+    public int insertUserFood(UserFood food, User user) {
         int id = 0;
+        int userId = user.getId();
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
         session.persist(food);
@@ -31,5 +46,25 @@ public class UserFoodDao {
         id = food.getId();
         session.close();
         return id;
+    }
+
+    public void deleteUserFood(UserFood food) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(food);
+        transaction.commit();
+        session.close();
+    }
+
+    public List<UserFood> getAllUserFood() {
+        Session session = sessionFactory.openSession();
+        HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<UserFood> criteria = builder.createQuery(UserFood.class);
+        Root<UserFood> root = criteria.from(UserFood.class);
+        List<UserFood> foods = session.createQuery(criteria).getResultList();
+
+        logger.debug("The list of foods is " + foods);
+        session.close();
+        return foods;
     }
 }
---
+++ src/test/java/edu/matc/persistence/UserFoodDaoTest.java
@@ -0,0 +1,28 @@
+package edu.matc.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserFoodDaoTest {
+
+    @Test
+    void getUserFoodById() {
+    }
+
+    @Test
+    void updateUserFood() {
+    }
+
+    @Test
+    void insertUserFood() {
+    }
+
+    @Test
+    void deleteUserFood() {
+    }
+
+    @Test
+    void getAllUserFood() {
+    }
+}
