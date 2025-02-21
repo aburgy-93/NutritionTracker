0de25545d1229665300e545dc81c3affbf4c57ce
aburgy-93 <aburgy@uwalumni.com>
2025-02-21 15:07:04 -0600

    Updated the UserFoodDaoTest to use the GenericDao class and deleted all daos not being used anymore

--- src/test/java/edu/matc/persistence/UserFoodDaoTest.java
+++ src/test/java/edu/matc/persistence/UserFoodDaoTest.java
@@ -7,23 +7,21 @@ import edu.matc.util.Database;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserFoodDaoTest {
-    UserFoodDao userFoodDao;
-    FoodDao foodDao;
-
     GenericDao<User> userDao;
-//    GenericDao<Food> foodDao;
-//    GenericDao<UserFood> userFoodDao;
+    GenericDao<Food> foodDao;
+    GenericDao<UserFood> userFoodDao;
 
 
     @BeforeEach
     void setUp() {
-        userFoodDao = new UserFoodDao();
-        foodDao = new FoodDao();
+        userFoodDao = new GenericDao<>(UserFood.class);
+        foodDao = new GenericDao<>(Food.class);
         userDao = new GenericDao<>(User.class);
 
         Database database = new Database();
@@ -32,50 +30,49 @@ class UserFoodDaoTest {
 
     @Test
     void getUserFoodById() {
-        UserFood retrievedUserFood = userFoodDao.getUserFoodById(1);
+        UserFood retrievedUserFood = userFoodDao.getById(1);
         assertNotNull(retrievedUserFood);
         assertEquals("Chicken Breast", retrievedUserFood.getFoodName());
     }
 
     @Test
     void updateUserFood() {
-        Food retrievedFood = foodDao.getFoodById(1);
+        Food retrievedFood = foodDao.getById(1);
         retrievedFood.setFoodName("Lean Chicken Breast");
-        foodDao.updateFood(retrievedFood);
-        UserFood retrievedUserFood = userFoodDao.getUserFoodById(1);
+        foodDao.update(retrievedFood);
+        UserFood retrievedUserFood = userFoodDao.getById(1);
         assertNotNull(retrievedUserFood);
         assertEquals("Lean Chicken Breast", retrievedUserFood.getFoodName());
     }
 
     @Test
-    void insertUserFood() {
+    void insertUserFood() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
         // get a user
         User user = userDao.getById(1);
         assertNotNull(user);
 
         // Create food with that user
         UserFood foodEnteredByUser = new UserFood(user.getId(), 2, "2025-02-20", 1, "Breakfast", 70, 6.0, 1.0, 4.5, user);
-        int insertedUserFoodId = userFoodDao.insertUserFood(foodEnteredByUser);
+        int insertedUserFoodId = userFoodDao.insert(foodEnteredByUser);
 
         // retrieve the food
-        UserFood retrievedUserFood = userFoodDao.getUserFoodById(insertedUserFoodId);
+        UserFood retrievedUserFood = userFoodDao.getById(insertedUserFoodId);
 
         // Verify
         assertNotNull(retrievedUserFood);
         assertEquals("White Rice", retrievedUserFood.getFoodName());
-
     }
 
     @Test
     // deletes everything a user tracked
     void deleteUserFood() {
-        userFoodDao.deleteUserFood(userFoodDao.getUserFoodById(2));
-        assertNull(userFoodDao.getUserFoodById(2));
+        userFoodDao.deleteEntity(userFoodDao.getById(2));
+        assertNull(userFoodDao.getById(2));
     }
 
     @Test
     void getAllUserFood() {
-        List<UserFood> foods = userFoodDao.getAllUserFood();
+        List<UserFood> foods = userFoodDao.getAll();
         assertNotNull(foods);
         assertEquals(2, foods.size());
     }
