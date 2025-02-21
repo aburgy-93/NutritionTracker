8c50951be038821da0ddf41e318438cda763e423
aburgy-93 <aburgy@uwalumni.com>
2025-02-21 14:54:22 -0600

    Refactored the FoodDaoTest to use the GenericDao class

--- src/test/java/edu/matc/persistence/FoodDaoTest.java
+++ src/test/java/edu/matc/persistence/FoodDaoTest.java
@@ -5,58 +5,57 @@ import edu.matc.util.Database;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FoodDaoTest {
-    FoodDao foodDao;
-
+    GenericDao genericDao;
 
     @BeforeEach
 
     void setUp() {
-        foodDao = new FoodDao();
+        genericDao = new GenericDao(Food.class);
         Database database = new Database();
         database.runSQL("cleanDB.sql");
     }
 
     @Test
     void getFoodById() {
-        Food retrievedFood = foodDao.getFoodById(1);
+        Food retrievedFood = (Food) genericDao.getById(1);
         assertNotNull(retrievedFood);
         assertEquals("Chicken Breast", retrievedFood.getFoodName());
     }
 
     @Test
     void getAllFood() {
-        List<Food> retrievedFood = foodDao.getAllFood();
+        List retrievedFood = genericDao.getAll();
         assertNotNull(retrievedFood);
         assertEquals(2, retrievedFood.size());
     }
 
     @Test
     void updateFood() {
-        Food foodToUpdate = foodDao.getFoodById(2);
+        Food foodToUpdate = (Food) genericDao.getById(1);
         foodToUpdate.setFoodName("Brown Rice");
-        foodDao.updateFood(foodToUpdate);
-
-        Food retrievedFood = foodDao.getFoodById(2);
+        genericDao.update(foodToUpdate);
+        Food retrievedFood = (Food) genericDao.getById(1);
         assertEquals("Brown Rice", retrievedFood.getFoodName());
     }
 
     @Test
-    void insertFood() {
+    void insertFood() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
         Food foodToInsert = new Food("Large Eggs", "Poultry", 1, "Large Egg",
                 70, 4.5,1.0,6.0);
-        foodDao.insertFood(foodToInsert);
+        int insertedFood = genericDao.insert(foodToInsert);
         assertEquals("Large Eggs", foodToInsert.getFoodName());
     }
 
     @Test
     void deleteFood() {
-        Food foodToDelete = foodDao.getFoodById(2);
-        foodDao.deleteFood(foodToDelete);
-        assertNull(foodDao.getFoodById(2));
+        Food foodToDelete = (Food) genericDao.getById(2);
+        genericDao.deleteEntity(foodToDelete);
+        assertNull(genericDao.getById(2));
     }
 }
