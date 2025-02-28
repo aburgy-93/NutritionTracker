2714bffec2082eaee5f75c2fe57403182b4e9033
aburgy-93 <aburgy@uwalumni.com>
2025-02-27 18:13:27 -0600

    Working on building the free macro calculator jsp

---
+++ src/main/java/edu/matc/controller/FoodTracker.java
@@ -0,0 +1,17 @@
+package edu.matc.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FoodTracker extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+
+        } catch (Exception e) {
+            log("Error processing request: " + e.getMessage(), e);
+        }
+    }
+}
---
+++ src/main/webapp/macro.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: alex
+  Date: 2/27/2025
+  Time: 3:40 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Results</title>
+</head>
+<body>
+  <h2>YOUR RESULTS</h2>
+</body>
+</html>
